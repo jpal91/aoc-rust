2bc7f22e28926ff373110ca498116e8b9505e16b
jpal91 <pallanschja@gmail.com>
2024-06-04 09:33:01 -0400

    Added a few more methods

--- grid/src/grid.rs
+++ grid/src/grid.rs
@@ -29,7 +29,7 @@ pub struct Grid<C> {
     n_neighbors: Sided,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Default)]
 pub struct Cell<T> {
     pub val: T,
     y: i32,
@@ -38,23 +38,21 @@ pub struct Cell<T> {
 
 pub struct GridIter<'i, C> {
     grid: &'i [C],
-    y: usize,
-    x: usize,
     idx: usize,
 }
 
-impl<C> Grid<C>
+impl<'g, C> Grid<C>
 where
-    C: CellLike,
+    C: CellLike<'g>,
 {
-    pub fn new(input: &'static str, neighbors: Sided) -> Self {
+    pub fn new(input: &'g str, neighbors: Sided) -> Self {
         let mut g = input
             .trim()
             .lines()
             .enumerate()
             .map(|(y, l)| {
                 l.trim()
-                    .split("")
+                    .split(' ')
                     .filter(|v| !v.is_empty())
                     .enumerate()
                     .map(|(x, c)| C::new_from_str(c, y as i32, x as i32))
@@ -101,11 +99,9 @@ where
         self.grid.iter()
     }
 
-    pub fn iter_enum(&self) -> impl Iterator<Item = (usize, usize, &C)> {
+    pub fn iter_enum(&'g self) -> impl Iterator<Item = (usize, usize, &C)> {
         GridIter {
             grid: &self.grid,
-            y: 0,
-            x: 0,
             idx: 0,
         }
     }
@@ -139,9 +135,9 @@ where
     }
 }
 
-impl<C> Index<(usize, usize)> for Grid<C>
+impl<'g, C> Index<(usize, usize)> for Grid<C>
 where
-    C: CellLike,
+    C: CellLike<'g>,
 {
     type Output = C;
 
@@ -151,9 +147,9 @@ where
     }
 }
 
-impl<C> IndexMut<(usize, usize)> for Grid<C>
+impl<'g, C> IndexMut<(usize, usize)> for Grid<C>
 where
-    C: CellLike,
+    C: CellLike<'g>,
 {
     fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
         let coords = (index.0 * self.rows) + index.1;
@@ -163,7 +159,7 @@ where
 
 impl<'i, C> Iterator for GridIter<'i, C>
 where
-    C: CellLike,
+    C: CellLike<'i>,
 {
     type Item = (usize, usize, &'i C);
 
@@ -173,14 +169,17 @@ where
         }
 
         let item = &self.grid[self.idx];
+
+        let (x, y) = item.coords();
+
         self.idx += 1;
 
-        Some((self.y, self.x, item))
+        Some((y, x, item))
     }
 }
 
-pub trait CellLike {
-    fn new_from_str(val: &'static str, y: i32, x: i32) -> Self;
+pub trait CellLike<'cell> {
+    fn new_from_str(val: &'cell str, y: i32, x: i32) -> Self;
 
     fn new_from_coords(self, y: usize, x: usize) -> Self;
 
@@ -200,7 +199,7 @@ macro_rules! impl_cell_like {
 
     ( $($type:ty $(,)?)* ) => {
         $(
-            impl CellLike for Cell<$type> {
+            impl CellLike<'_> for Cell<$type> {
                 fn new_from_str(val: &str, y: i32, x: i32) -> Self {
                     Cell {
                         val: val.trim().parse().unwrap_or_default(),
@@ -225,8 +224,28 @@ macro_rules! impl_cell_like {
 
 impl_cell_like!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
 
-impl<'cell: 'static> CellLike for Cell<&'cell str> {
-    fn new_from_str(val: &'static str, y: i32, x: i32) -> Self {
+impl CellLike<'_> for Cell<String> {
+    fn new_from_str(val: &str, y: i32, x: i32) -> Self {
+        Cell {
+            val: val.to_owned(),
+            y,
+            x,
+        }
+    }
+    fn new_from_coords(self, y: usize, x: usize) -> Self {
+        Cell {
+            val: self.val,
+            y: y as i32,
+            x: x as i32,
+        }
+    }
+    fn coords(&self) -> (usize, usize) {
+        (self.x as usize, self.y as usize)
+    }
+}
+
+impl<'cell> CellLike<'cell> for Cell<&'cell str> {
+    fn new_from_str(val: &'cell str, y: i32, x: i32) -> Self {
         Cell { val, y, x }
     }
     fn new_from_coords(self, y: usize, x: usize) -> Self {
@@ -238,5 +257,86 @@ impl<'cell: 'static> CellLike for Cell<&'cell str> {
     }
     fn coords(&self) -> (usize, usize) {
         (self.x as usize, self.y as usize)
+    }
+}
+
+#[cfg(test)]
+const TEST_GRID: &str = "\
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+";
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn expected<'e, T>(rows: usize, cols: usize) -> Grid<Cell<T>>
+    where
+        Cell<T>: CellLike<'e>,
+    {
+        Grid {
+            grid: (0..rows)
+                .flat_map(|i| (0..cols).map(move |j| Cell::new_from_str("0", i as i32, j as i32)))
+                .collect(),
+            rows,
+            cols,
+            n_neighbors: Sided::Four,
+        }
+    }
+
+    #[test]
+    fn basic_grid() {
+        let grid: DefaultGrid<u32> = Grid::new(TEST_GRID, Sided::Four);
+        let expected = expected::<u32>(5, 5);
+        assert_eq!(grid, expected);
+    }
+
+    #[test]
+    fn odd_size() {
+        let grid: DefaultGrid<u32> = Grid::new(
+            &TEST_GRID.lines().take(4).collect::<Vec<_>>().join("\n"),
+            Sided::Four,
+        );
+        let expected = expected::<u32>(4, 5);
+        assert_eq!(grid, expected);
+    }
+
+    #[test]
+    fn diff_types() {
+        let grid = DefaultGrid::<u8>::new(TEST_GRID, Sided::Four);
+        let expect = expected::<u8>(5, 5);
+        assert_eq!(grid, expect);
+
+        let grid: DefaultGrid<i16> = Grid::new(TEST_GRID, Sided::Four);
+        let expect = expected::<i16>(5, 5);
+        assert_eq!(grid, expect);
+
+        let grid: DefaultGrid<String> = Grid::new(TEST_GRID, Sided::Four);
+        let expect = expected::<String>(5, 5);
+        assert_eq!(grid, expect);
+
+        let grid: DefaultGrid<&str> = Grid::new(TEST_GRID, Sided::Four);
+        let expect = expected::<&str>(5, 5);
+        assert_eq!(grid, expect);
+    }
+
+    #[test]
+    fn test_iter_enum() {
+        let grid = DefaultGrid::<u8>::new(TEST_GRID, Sided::Four);
+
+        let mut iter = grid.iter_enum();
+
+        let mut last: (usize, usize, &Cell<u8>) = iter.next().unwrap();
+
+        assert_eq!(last, (0, 0, &Cell { val: 0, y: 0, x: 0 }));
+
+        for i in iter {
+            last = i;
+        }
+
+        assert_eq!(last, (4, 4, &Cell { val: 0, y: 4, x: 4 }));
     }
 }
