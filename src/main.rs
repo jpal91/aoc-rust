fc38ed279855408d98c7ffba247a1aa8debb207b
jpal91 <pallanschja@gmail.com>
2024-06-05 17:29:59 -0400

    quick fix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 
-use std::env;
 use std::collections::HashMap;
+use std::env;
 use std::fs::read_to_string;
 use std::path::PathBuf;
 // use std::process::Command;
@@ -15,12 +15,12 @@ pub mod year2022 {
 pub mod year2023 {
     pub mod day1;
     pub mod day2;
+    pub mod day23;
     pub mod day3;
     pub mod day4;
     pub mod day5;
     pub mod day6;
     pub mod day7;
-    pub mod day23;
 }
 
 pub mod utils {
@@ -28,20 +28,17 @@ pub mod utils {
 }
 
 macro_rules! solution {
-    ($year:tt, $day:tt) => {
-        {
-            use $year::$day::main as solution_main;
-            solution_main as fn()
-        }
-    };
+    ($year:tt, $day:tt) => {{
+        use $year::$day::main as solution_main;
+        solution_main as fn()
+    }};
 }
-
 
 #[macro_export]
 macro_rules! time_it {
     ($context:literal, $s:stmt) => {
         let timer = std::time::Instant::now();
-        let res = {$s};
+        let res = { $s };
         println!("{}: {:?}\n{:?}", $context, timer.elapsed(), res);
     };
 }
@@ -65,14 +62,20 @@ fn y2023(day: &str) -> () {
         ("5", solution!(year2023, day5)),
         ("6", solution!(year2023, day6)),
         ("7", solution!(year2023, day7)),
-        ("23", solution!(year2023, day23))
+        ("23", solution!(year2023, day23)),
     ]);
 
     solutions_map.get(day).expect("Not in solutions map")();
 }
 
 pub fn get_puzzle(year: &str, day: &str) -> String {
-    let path: PathBuf = ["input", format!("y{}", year).as_str(), format!("day{}.txt", day).as_str()].iter().collect();
+    let path: PathBuf = [
+        "input",
+        format!("y{}", year).as_str(),
+        format!("day{}.txt", day).as_str(),
+    ]
+    .iter()
+    .collect();
     let data = read_to_string(&path).expect("Not there");
 
     data
@@ -86,10 +89,8 @@ fn main() {
     };
     let day: &String = &args[1];
     println!("{} {}", "Running Day", &day);
-    
+
     // solution!(year2023, day1)
-    // y2023(&day)
-    y2022(&day)
-
+    y2023(&day)
+    // y2022(&day)
 }
-
