83d6542bd988ab8fb075923a82f07086f088e113
jpal91 <pallanschja@gmail.com>
2024-06-06 10:57:48 -0400

    Cleanup and refactored main function

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,58 @@
 #![allow(unused)]
 
+use clap::Parser;
+use paste::paste;
 use std::collections::HashMap;
 use std::env;
-use std::fs::read_to_string;
+use std::fs::{self, read_to_string};
 use std::path::PathBuf;
-// use std::process::Command;
 
-pub mod year2022 {
-    pub mod day1;
-    pub mod day2;
-    pub mod day3;
+#[derive(Parser, Debug)]
+struct Args {
+    /// Year to work with
+    #[arg(short, long, default_value = "2023")]
+    year: u32,
+
+    /// Target day
+    day: String,
 }
 
-pub mod year2023 {
-    pub mod day1;
-    pub mod day2;
-    pub mod day23;
-    pub mod day3;
-    pub mod day4;
-    pub mod day5;
-    pub mod day6;
-    pub mod day7;
+macro_rules! aoc {
+    () => {};
+
+    ( $( $year:literal => [ $($days:literal $(,)?)* ] $(,)? )* ) => {
+        paste! {
+            $(
+                pub mod [<year$year>]  {
+                    $(
+                        pub mod [<day$days>];
+                    )*
+                }
+
+                fn [<y$year>](day: &str) {
+                    let solutions_map = ::std::collections::HashMap::from([
+                        $(
+                            (
+                                stringify!($days),
+                                {
+                                    use [<year$year>]::[<day$days>]::main as solution_main;
+                                    solution_main as fn()
+                                }
+                            )
+                        ),*
+                    ]);
+
+                    solutions_map.get(day).expect("Not in solutions map")();
+                }
+            )*
+        }
+    };
 }
 
-pub mod utils {
-    pub mod grid;
-}
-
-macro_rules! solution {
-    ($year:tt, $day:tt) => {{
-        use $year::$day::main as solution_main;
-        solution_main as fn()
-    }};
-}
+aoc!(
+    2022 => [1, 2, 3],
+    2023 => [1, 2, 3, 4, 5, 6, 7, 23]
+);
 
 #[macro_export]
 macro_rules! time_it {
@@ -43,52 +63,26 @@ macro_rules! time_it {
     };
 }
 
-fn y2022(day: &str) {
-    let solutions_map = HashMap::from([
-        ("1", solution!(year2022, day1)),
-        ("2", solution!(year2022, day2)),
-        ("3", solution!(year2022, day3)),
-    ]);
-
-    solutions_map.get(day).expect("Not in solutions map")();
-}
-
-fn y2023(day: &str) {
-    let solutions_map = HashMap::from([
-        ("1", solution!(year2023, day1)),
-        ("2", solution!(year2023, day2)),
-        ("3", solution!(year2023, day3)),
-        ("4", solution!(year2023, day4)),
-        ("5", solution!(year2023, day5)),
-        ("6", solution!(year2023, day6)),
-        ("7", solution!(year2023, day7)),
-        ("23", solution!(year2023, day23)),
-    ]);
-
-    solutions_map.get(day).expect("Not in solutions map")();
+pub fn debug_output<O: AsRef<str>>(output: O, year: usize, day: usize) {
+    let path = PathBuf::from_iter(["debug", &format!("y{year}/day{day}.txt")]);
+    fs::write(path, output.as_ref()).unwrap();
 }
 
 pub fn get_puzzle(year: &str, day: &str) -> String {
-    let path: PathBuf = [
-        "input",
-        format!("y{}", year).as_str(),
-        format!("day{}.txt", day).as_str(),
-    ]
-    .iter()
-    .collect();
+    let path = PathBuf::from_iter(["input", &format!("y{year}/day{day}.txt")]);
     read_to_string(path).expect("Not there")
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() < 2 {
-        panic!("Need more args")
+    let year = match args.year {
+        2023 | 23 => y2023,
+        2022 | 22 => y2022,
+        y => panic!("Invalid year {}", y),
     };
-    let day: &String = &args[1];
-    println!("Running day {}", &day);
 
-    // solution!(year2023, day1)
-    y2023(day)
-    // y2022(&day)
+    println!("Running Day: {}, Year: {}", &args.day, args.year);
+
+    year(&args.day);
 }
--- src/year2023/day7.rs
+++ src/year2023/day7.rs
@@ -1,1 +1,236 @@
-pub fn main() {}
+use crate::{debug_output, get_puzzle, time_it};
+use std::cmp::Reverse;
+use std::sync::{Mutex, OnceLock};
+use std::{collections::HashMap, ops::Deref};
+
+#[derive(Eq, Debug, Clone, PartialEq)]
+enum Card {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Ten,
+    Num(u32),
+    Joker,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq, Clone)]
+enum HandType {
+    FiveOK = 7,
+    FourOK,
+    FullHouse,
+    ThreeOK,
+    TwoPair,
+    Pair,
+    High,
+}
+
+#[derive(Debug, Clone, Ord, PartialEq, PartialOrd, Eq)]
+struct Hand(Vec<Card>);
+
+#[derive(Debug)]
+struct Player {
+    hand: Hand,
+    bid: u32,
+}
+
+impl Hand {
+    fn new(input: &str, part2: bool) -> Self {
+        Self(input.chars().map(|c| Card::new(c, part2)).collect())
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.deref().cmp(self.deref())
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Deref for Card {
+    type Target = u32;
+
+    fn deref(&self) -> &Self::Target {
+        use Card::*;
+
+        match self {
+            Ace => &14,
+            King => &13,
+            Queen => &12,
+            Jack => &11,
+            Ten => &10,
+            Joker => &0,
+            Num(n) => n,
+        }
+    }
+}
+
+impl Deref for Hand {
+    type Target = [Card];
+
+    fn deref(&self) -> &Self::Target {
+        self.0.as_slice()
+    }
+}
+
+impl Card {
+    fn new(card: char, part2: bool) -> Self {
+        use Card::*;
+
+        match card {
+            'A' => Ace,
+            'K' => King,
+            'Q' => Queen,
+            'J' if part2 => Joker,
+            'J' => Jack,
+            'T' => Ten,
+            a => Num(a.to_string().parse().unwrap()),
+        }
+    }
+}
+
+impl From<i32> for Card {
+    fn from(value: i32) -> Self {
+        use Card::*;
+
+        match value {
+            14 => Ace,
+            13 => King,
+            12 => Queen,
+            11 => Jack,
+            10 => Ten,
+            0 => Joker,
+            any => Num(any as u32),
+        }
+    }
+}
+
+impl HandType {
+    fn new(mut hand: impl Iterator<Item = i32>) -> Self {
+        use HandType::*;
+        if let Some(card) = hand.next() {
+            match card {
+                5 => return FiveOK,
+                4 => return FourOK,
+                3 => {
+                    if let Some(next) = hand.next() {
+                        match next {
+                            2 => return FullHouse,
+                            1 => return ThreeOK,
+                            _ => unreachable!(),
+                        }
+                    } else {
+                        return ThreeOK;
+                    }
+                }
+                2 => {
+                    if let Some(next) = hand.next() {
+                        match next {
+                            2 => return TwoPair,
+                            1 => return Pair,
+                            n => unreachable!("{}", n),
+                        }
+                    } else {
+                        return Pair;
+                    }
+                }
+                1 => return High,
+                _ => unreachable!(),
+            }
+        }
+
+        High
+    }
+}
+
+fn counter(hand: &Hand) -> HandType {
+    let mut cards: HashMap<i32, i32> = HashMap::new();
+    hand.iter().for_each(|c| {
+        cards.entry(**c as i32).and_modify(|e| *e += 1).or_insert(1);
+    });
+
+    let cards = match cards.remove(&0) {
+        Some(5) => vec![5],
+        Some(joker) => {
+            let mut cards: Vec<i32> = cards.values().copied().collect();
+            cards.sort_by_key(|k| -k);
+            cards[0] = (cards[0] + joker).min(5);
+            cards
+        }
+        None => {
+            let mut cards: Vec<i32> = cards.values().copied().collect();
+            cards.sort_by_key(|k| -k);
+            cards
+        }
+    };
+
+    HandType::new(cards.into_iter())
+    // hand_type
+}
+
+fn parse(input: &str, part2: bool) -> Vec<Player> {
+    input
+        .lines()
+        .map(|line| {
+            let mut player = line.split(' ');
+            let mut hand = Hand::new(player.next().unwrap(), part2);
+            let bid = player.next().unwrap().trim().parse().unwrap();
+            Player { hand, bid }
+        })
+        .collect()
+}
+
+fn solution(input: &str, part2: bool) -> u32 {
+    let players = parse(input, part2);
+    let mut hands = players
+        .into_iter()
+        .map(|p| (p.bid, counter(&p.hand), p.hand.clone()))
+        .collect::<Vec<_>>();
+    hands.sort_by_key(|a| Reverse((a.1.clone(), a.2.clone())));
+
+    hands
+        .into_iter()
+        .map(|h| h.0)
+        .enumerate()
+        .fold(0, |acc, (i, bid)| acc + (bid * (i as u32 + 1)))
+}
+
+pub fn main() {
+    let puzzle = get_puzzle("23", "7");
+
+    time_it!("Part 1", solution(&puzzle, false));
+    time_it!("Part 2", solution(&puzzle, true));
+}
+
+#[cfg(test)]
+const TEST_ONE: &str = "\
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+";
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+        let res = solution(TEST_ONE, false);
+
+        assert_eq!(res, 6440)
+    }
+
+    #[test]
+    fn test_two() {
+        let res = solution(TEST_ONE, true);
+
+        assert_eq!(res, 5905)
+    }
+}
